5728d80ac8379c70c54d821cd8143ba5863446c8
Zekechan11 <145641228+Zekechan11@users.noreply.github.com>
2024-05-10 20:27:54 +0800

    usab 1.0

--- src/lib/services/app.js
+++ src/lib/services/app.js
@@ -57,9 +57,9 @@ checkSigninInput = () => {
 }
 
 // darkmode toggle
-// darkmode_toggle.onclick = (e) => {
-//     e.preventDefault()
-//     let body = document.querySelector('body')
-//     body.classList.toggle('dark')
-//     darkmode_toggle.innerHTML = body.classList.contains('dark') ? 'Lightmode' : 'Darkmode'
-// }
+darkmode_toggle.onclick = (e) => {
+    e.preventDefault()
+    let body = document.querySelector('body')
+    body.classList.toggle('dark')
+    darkmode_toggle.innerHTML = body.classList.contains('dark') ? 'Lightmode' : 'Darkmode'
+}
